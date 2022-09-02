85be5c801258c1aed7c86631f88e0f538546baa1
ahmedullah12 <gulamjoy321@gmail.com>
2022-09-02 21:34:05 +0600

    categories added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("app.js")
+const loadCategories = () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories';
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategories(data.data.news_category))
+}
+
+
+const displayCategories = (data) => {
+    const categoriesSection = document.getElementById('categories-section');
+    data.forEach(category => {
+        const categoryName = document.createTextNode(category.category_name);
+        
+        const categoryLink = document.createElement('a');
+        // categoryDiv.classList.add('')
+        categoryLink.classList.add("text-decoration-none", "text-secondary")
+        categoryLink.appendChild(categoryName);
+        categoryLink.href = "https://www.google.com";
+
+        categoriesSection.appendChild(categoryLink)
+
+    });
+}  
+
+loadCategories();
